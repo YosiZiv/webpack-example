af4de06bb59303aa772bfcd66c4128ed94dc3e54
yosi ziv <yosizivjs@gmail.com>
2020-12-25 13:11:14 +0200

    build test success

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
+require("babel-runtime/regenerator")
 require("./main.css")
 require('./index.html')
+
+const babelTest = async (args) => {
+    const {a,b} = args
+    await console.log("test", a,b)
+
+}
+babelTest({a: 1, b: 2})
