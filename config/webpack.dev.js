555f138b7fa7335f96bff865609a50abc5308920
yosi ziv <yosizivjs@gmail.com>
2020-12-25 10:19:09 +0200

    init application

---
+++ config/webpack.dev.js
@@ -0,0 +1,30 @@
+const path = require("path")
+module.exports = {
+    entry: {
+        main: "./src/main.js"
+    },
+    mode: "development",
+    output: {
+        filename: "[name]-bundle.js",
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: "/"
+    },
+    devServer: {
+        contentBase: "dist"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            }
+        ]
+    }
+}
